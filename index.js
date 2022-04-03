8a490fe59bdcf41a1f77374a5daeb4827dcbc752
Cory <cameyer182@gmail.com>
2022-04-02 21:08:34 -0400

    commit tree 709de63085b1c0d06197eccdc0691e71c941c013

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    let negativizedArray = []
+    sourceArray.forEach(num => negativizedArray.push(num * -1));
+    return negativizedArray;
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+    let doubledArray = []
+    sourceArray.forEach(num => doubledArray.push(num * 2))
+    return doubledArray;
+}
+
+function mapToSquare(sourceArray) {
+    let squaredArray = []
+    sourceArray.forEach(num => squaredArray.push(num ** 2))
+    return squaredArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint;
+    for (const num of sourceArray) {
+        total += num;
+    };
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    let trueOrFalse = true;
+    for (const word of sourceArray) {
+        trueOrFalse = (trueOrFalse && !!word)
+    };
+    return trueOrFalse;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (const word of sourceArray) {
+        if (!!word) {return true}
+    };
+    return false;
+}
